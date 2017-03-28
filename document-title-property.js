81d5875a204463414ae34c5bb2b9d699bbf39033
Lie Ryan <lie.1296@gmail.com>
2017-03-28 23:23:35 +1100

    Rewritten to use content_script instead of background script
    This allows us to listen for document.title changes and update the title
    accordingly

---
+++ document-title-property.js
@@ -0,0 +1,51 @@
+class TitleManager {
+  constructor() {
+    // title property descriptor before we override it
+    let originalTitleDescriptor = Object.getOwnPropertyDescriptor(Document.prototype, "title");
+    this.getTitle = originalTitleDescriptor.get.bind(document);
+    this.setTitle = originalTitleDescriptor.set.bind(document);
+  }
+
+  attachListeners() {
+    let self = this;
+    let unsafeDocument = document.wrappedJSObject;
+    let unsafeDocumentPrototype = unsafeDocument.__proto__;
+
+    let getter = exportFunction(function() {
+      return self.realTitle;
+    }, unsafeDocumentPrototype);
+
+    let setter = exportFunction(function(newVal) { 
+      console.log('watched'); 
+      if (!(newVal instanceof String || typeof newVal === "string")) {
+        // TODO: safely newVal.toString() in the page's security context
+        console.error('add-url-to-window-title:', 'page script trying to set non-string to document.title is unsupported');
+        return;
+      }
+      self.realTitle = newVal;
+    }, unsafeDocumentPrototype);
+
+    Object.defineProperty(unsafeDocumentPrototype, 'title', { 'configurable': true, 'get': getter, 'set': setter});
+    console.log('attached');
+  }
+
+  updateTitle() {
+    let urledTitle = config.getTitle(this.realTitle, document.URL);
+    console.log('updating title', urledTitle);
+    this.setTitle(urledTitle);
+  }
+
+  get realTitle() {
+    return this._realTitle;
+  }
+
+  set realTitle(newVal) {
+    console.log('write');
+    this._realTitle = newVal;
+    this.updateTitle();
+  }
+};
+let tm = new TitleManager();
+tm.attachListeners();
+tm.realTitle = document.title;
+console.log('injected: document-title-property.js');
